6e7abdc8b378ac537d52c0495483cc44f7afa2c4
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-02-11 00:45:26 -0700

    Add files via upload

--- auto.js
+++ auto.js
@@ -1,6 +1,8 @@
 let img1 = document.getElementById('image1')
 let img2 = document.getElementById('image2')
 let img3 = document.getElementById('image3')
+let btn1 = document.getElementById('btn1')
+//let btn2 = documen.getElementById('btn2')
 
 function enter1(){
     window.open("https://www.instagram.com/x.___rinshad___.x/?next=%2F")
@@ -14,4 +16,11 @@ function enter3(){
     window.open("https://www.linkedin.com/in/mohammed-rinshad-77424b288/")
 }
 
+function enter() {
+    window.open ('auto2.html')
+}
+
+function enter4(){
+    window.open ('auto3.html')
+}
 
